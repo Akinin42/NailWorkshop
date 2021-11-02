ef0badf445d3d2c5ff15b1aa1ff96acdd8400fbb
Igor Akinin <akinin32167@gmail.com>
2021-11-02 19:06:29 +0700

    initial commit

---
+++ NailService/src/main/java/org/nailservice/NailServiceApplication.java
@@ -0,0 +1,13 @@
+package org.nailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailServiceApplication.class, args);
+	}
+
+}
